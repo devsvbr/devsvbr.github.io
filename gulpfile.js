35e727f500047619d134e27cb9cab9d3a7ec8155
Eric Yuzo <yuzo.devsv@gmail.com>
2017-04-14 12:14:38 -0300

    ajustando indentacao

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var jpegRecompress = require('imagemin-jpeg-recompress');
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    jpegRecompress = require('imagemin-jpeg-recompress');
 
 gulp.task('optimize-img', function() {
   return gulp.src('_img/**/*')
-    .pipe(imagemin([
-        imagemin.svgo({
+      .pipe(imagemin([
+          imagemin.svgo({
             plugins: [{removeViewBox: false}]
-        }),
-        pngquant({
+          }),
+          pngquant({
             quality: '65-80',
             speed: 1
-        }),
-        jpegRecompress({
+          }),
+          jpegRecompress({
             progressive: true,
             max: 85,
             min: 80
-        })
-    ]))
-    .pipe(gulp.dest('img'));
+          })
+      ]))
+      .pipe(gulp.dest('img'));
 });
