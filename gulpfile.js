f327f0af5cad801143c0546a058828a96ab1b8a5
Eric Yuzo <yuzo.devsv@gmail.com>
2017-04-13 23:01:58 -0300

    usando jpeg-recompress pra otimizar JPEGs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var jpegRecompress = require('imagemin-jpeg-recompress');
 
 gulp.task('optimize-img', function() {
   return gulp.src('_gulp/img/**/*')
@@ -11,6 +12,11 @@ gulp.task('optimize-img', function() {
         pngquant({
             quality: '65-80',
             speed: 1
+        }),
+        jpegRecompress({
+            progressive: true,
+            max: 85,
+            min: 80
         })
     ]))
     .pipe(gulp.dest('img'));
