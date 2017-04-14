b029e940b9f0b71239fcf0c3bfa99661b375cbfd
Eric Yuzo <yuzo.devsv@gmail.com>
2017-04-13 22:57:50 -0300

    usando pngquant pra otimizar PNGs

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 gulp.task('optimize-img', function() {
   return gulp.src('_gulp/img/**/*')
     .pipe(imagemin([
         imagemin.svgo({
             plugins: [{removeViewBox: false}]
+        }),
+        pngquant({
+            quality: '65-80',
+            speed: 1
         })
     ]))
     .pipe(gulp.dest('img'));
