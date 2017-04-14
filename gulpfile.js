535f2b0b4816897dfdb73fa3e3e0e6187bef2e8f
Eric Yuzo <yuzo.devsv@gmail.com>
2017-04-13 22:55:31 -0300

    criando task pra otimizar imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('optimize-img', function() {
+  return gulp.src('_gulp/img/**/*')
+    .pipe(imagemin([
+        imagemin.svgo({
+            plugins: [{removeViewBox: false}]
+        })
+    ]))
+    .pipe(gulp.dest('img'));
+});
