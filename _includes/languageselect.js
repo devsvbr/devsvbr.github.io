0c141aaf4dc82908ee6b8eb88370482173db116f
Eric Yuzo <eyuzo@devsv.com.br>
2019-04-29 20:41:31 -0300

    incluindo languageselector.js minificado

---
+++ _includes/languageselect.js
@@ -0,0 +1,1 @@
+var languageSelect=document.getElementById("footer-language-select");languageSelect&&(languageSelect.onchange=function(){window.location=languageSelect.options[languageSelect.selectedIndex].value});
